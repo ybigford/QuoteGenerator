141ed46f8a589c141cd3bd01b3cc19afa4372cf3
Yapa Bigford <110362211+ybigford@users.noreply.github.com>
2022-08-19 18:42:57 -0700

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,28 @@
+let btn = document.getElementById('btn');
+output = document.getElementById('output');
+let quotes =
+    [
+        '"Life is not about waiting for the storm to pass, its about learning to dance in the rain." - Vivian Greene',
+        '"If you get up in the morning and think the future is going to be better, it is a bright day. Otherwise, its not." - Elon Musk',
+        '"The art of life is the art of avoiding pain." - Thomas Jefferson',
+        '"Life is like riding a bicycle. To keep your balance, you must keep moving." — Albert Einstein',
+        '“If you tell the truth, you do not have to remember anything.” ― Mark Twain',
+        '“A friend is someone who knows all about you and still loves you.” ― Elbert Hubbard',
+        '"When you reach the end of your rope, tie a knot in it and hang on." - Franklin D Roosevelt',
+        '"The future belongs to those who believe in the beauty of their dreams." - Eleanor Roosevelt',
+        '"It is during our darkest moments that we must focus to see the light." - Aristotle',
+        '"Life is really simple, but we insist on making it complicated." - Confucius',
+        '"The secret of success is to do the common thing uncommonly well." - John D. Rockefeller Jr.',
+        '"If you are not willing to risk the usual, you will have to settle for the ordinary." - Jim Rohn',
+        '"You miss 100% of the shots you do not take." - Wayne Gretzky',
+        ' "Believe you can and you are halfway there." - Theodore Roosevelt', 
+        
+
+
+    ];
+
+btn.addEventListener('click', function(){
+    let randomQuote = quotes[Math.floor(Math.random() * quotes.length)] 
+    output.innerHTML = randomQuote;
+    
+})
